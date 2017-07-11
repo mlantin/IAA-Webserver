e612e29f6052b56a890766e5a19060e8c11cf143
Hongzhu Zhao <zhaohongzhu@live.com>
2017-07-10 17:50:24 -0700

    Load whold scene to server. Delete temp folder on quit

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ var fs = require('fs');
 var wav = require('wav');
 var path = require('path');
 
+process.on('SIGINT', function() {
+  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+  var path = 'public/temp'
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+};
+  process.exit();
+})
+try{
+    fs.mkdirSync('public/temp');
+} catch(e) {}
+
+
 var ScenePath = 'public/scene/';
 var parserOptions = {
   inflate: true,
@@ -42,60 +63,76 @@ loadSceneFiles();
 
 app.use(bodyParser.raw(parserOptions)); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(function (req, res, next) {
+    var filename = path.basename(req.url);
+    var extension = path.extname(filename);
+    // if (extension === '.css')
+        console.log(req.url + " was requested.");
+    next();
+});
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-    console.log('Incoming GET request for public/audio/' + req.query.fn + '.wav');
-    fs.readFile('public/audio/' + req.query.fn + '.wav', function(err, data) {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          var message = 'Warning: No such file ' + err.path;
-          console.log(message);
-          res.status(404).send(message);
-        } else {
-          throw err;
-        }
-      }
-      res.send(data);
-  });
-});
-
-app.put('/audio', function (req, res) {
+app.put('/upload_audio', function (req, res) {
   var filename = req.query.fn+".wav";
   var writer = new wav.Writer(wavoptions);
-  writer.pipe(fs.createWriteStream("public/audio/"+filename));
+  writer.pipe(fs.createWriteStream(path.join("public", filename)));
   writer.write(req.body);
   writer.end();
   res.send('saved your audio file');
 });
 
-app.put('/scenes/', function (req, res) {
-
-});
-
-app.delete('/', function (req, res) {
-  console.log('Received delete request');
-  fs.unlink('public/audio/' + req.query.fn + '.wav', function (err) {
-    var message;
-    if (err) {
-        if (err.code === 'ENOENT') {
-          message = 'File ' + req.query.fn + '.wav does not exist';
-        } else {
-          throw err;
-        }
-    } else {
-      message = 'File ' + req.query.fn + '.wav deleted successfully';
+GenScene = function(filePath, fileContent) {
+    // console.log(String(fileContent));
+    var scene = JSON.parse(fileContent);
+    var sceneName = path.basename(filePath).slice(0, -5);
+    var dir = path.join('public/scene', sceneName);
+    if (fs.existsSync(dir)) {
+        throw 'Directory already exists.'
     }
-    console.log(message);
-    res.send(message);
-  });
+    fs.mkdirSync(dir);
+    fs.mkdirSync(path.join(dir, 'audio'));
+    try {
+        scene.wordInfoList.forEach(
+            function(item, index, theArray) {
+                var audioFile = item.clipfn;
+                var oriPath = path.join('public', audioFile + '.wav');
+                var desPath = path.join(dir, 'audio', path.basename(audioFile) + '.wav');
+                item.clipfn = path.join('scene', sceneName, 'audio', path.basename(audioFile));
+                fs.createReadStream(oriPath).pipe(fs.createWriteStream(desPath));
+            }
+        )
+        fs.writeFileSync(path.join(dir, 'config.json'), JSON.stringify(scene, null, 4));
+        loadSceneFiles();
+    } catch (e) {
+        console.log(e);
+        fs.rmdirSync(dir);
+        throw(e);
+    }
+};
+
+app.put('/scene', function (req, res) {
+    // req.body
+    var filename = path.join('public/temp', req.query.fn) + ".json";
+    fs.writeFile(filename, req.body, (err) => {
+        if (err) {
+            console.log("Save " + filename + " Failed");
+            res.status(400).send('Failed to save scene.(Save error)');
+        }
+        console.log('The file has been saved!');
+        try {
+            GenScene(filename, req.body);
+            res.send('Scene file' + req.query.fn + 'saved.');
+        } catch(e) {
+            res.status(400).send('Failed to save scene.(Collect error)');
+        }
+    });
+
 });
 
 app.get('/scenes/', function (req, res) {
     console.log('Request for scenes list.');
     res.json(filesArr);
 });
-
 
 app.listen(3000, function () {
   console.log('Audio Server listening on port 3000!')
